d06d1cd05cd9f304c7da121a599c889ca89f0d4a
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-29 13:39:15 +0000

    INPROGRESS setup;

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import 'phaser';
+
+declare var Phaser;
+
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'main' });
+  }
+  create() {
+    console.log('create method');
+  }
+  preload() {
+    console.log('preload method');
+  }
+  update() {
+    console.log('update method');
+  }
+}
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+//
+
